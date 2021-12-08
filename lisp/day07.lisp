7a56a297903998f05cb35f721b60cffe9c8e5d23
Jared March <jared.przybylski@gmail.com>
2021-12-07 17:08:11 -0800

    Cleanup on day 7.

--- lisp/day07.lisp
+++ lisp/day07.lisp
@@ -5,10 +5,9 @@
 (defun read-numbers (stream)
   (let ((*readtable* (copy-readtable)))
     (set-macro-character #\, #'ignore-commas)
-    (loop
-      for num = (read stream nil)
-      while num
-      collect num)))
+    (loop for num = (read stream nil)
+          while num
+          collect num)))
 
 (defun difference (number-1 number-2)
   (abs (- number-1 number-2)))
@@ -20,43 +19,22 @@
 (defun advent-7-1 ()
   (let* ((positions (with-open-file (stream "../input/7/input")
                       (read-numbers stream)))
-         (range-min (loop with min = nil
-                          for position in positions
-                          when (or (null min) (< position min))
-                            do (setf min position)
-                          finally (return min)))
-         (range-max (loop with max = nil
-                          for position in positions
-                          when (or (null max) (< max position))
-                            do (setf max position)
-                          finally (return max))))
+         (range-min (reduce #'min positions))
+         (range-max (reduce #'max positions)))
     (reduce #'min
             (loop
               for i from range-min to range-max
-              collect (loop
-                        with out = 0
-                        for position in positions
-                        do (incf out (difference position i))
-                           finally (return out))))))
+              collect (reduce #'+ (mapcar (lambda (p) (difference p i))
+                                         positions))))))
+
 
 (defun advent-7-2 ()
   (let* ((positions (with-open-file (stream "../input/7/input")
                       (read-numbers stream)))
-         (range-min (loop with min = nil
-                          for position in positions
-                          when (or (null min) (< position min))
-                            do (setf min position)
-                          finally (return min)))
-         (range-max (loop with max = nil
-                          for position in positions
-                          when (or (null max) (< max position))
-                            do (setf max position)
-                          finally (return max))))
+         (range-min (reduce #'min positions))
+         (range-max (reduce #'max positions)))
     (reduce #'min
             (loop
               for i from range-min to range-max
-              collect (loop
-                        with out = 0
-                        for position in positions
-                        do (incf out (increasing-difference position i))
-                           finally (return out))))))
+              collect (reduce #'+ (mapcar (lambda (p) (increasing-difference p i))
+                                         positions))))))
